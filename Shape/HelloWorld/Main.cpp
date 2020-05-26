9482893f7b8279a46decf6bd4ae0d7017de9294b
Richard-Hartwig <Richard_Hartwig@hotmail.com>
2020-05-25 22:46:05 -0400

    checking positive number

--- Shape/HelloWorld/Main.cpp
+++ Shape/HelloWorld/Main.cpp
@@ -6,18 +6,19 @@
 
 using namespace std;
 
-int doShape(int &ch); 
+int doShape(int ch); 
 void menu();
 void cinClear();
+double checkIfPositive(double input);
 
 int main() {
 
-	//Shape *shape;
 
-	cout << "I can find the area and perimeter of a shape.\n\nWhich shape would you like to find?\n";
+	cout << "I can find the area and perimeter of a shape.\n\nWhich shape would you like to find?\n\nI can only compute positive numbers.\n\n";
 	menu();
 
 	int ch;
+	
 	
 
 	while (!(cin >> ch) || ch > 6 || ch < 1) {
@@ -26,8 +27,7 @@ int main() {
 	}
 
 	doShape(ch);
-	//cinClear();
-			
+				
 	cout << "\nDo you want to try another?\n\nPress 'Y' or 'N'\n";
 	char cAnotherOne = NULL;	
 
@@ -45,8 +45,7 @@ int main() {
 	while (cAnotherOne == 'y' || cAnotherOne =='Y') {
 		system("cls");
 		cout << "Do you want to see what shapes we offer? Press 'Y' or 'N'\n \n";
-	//	cin >> cMenu;
-//cMenu = toupper(cMenu);
+
 
 		while (cMenu != 'y' || cMenu != 'Y' || cMenu != 'N' || cMenu != 'n') {
 			cinClear();
@@ -57,24 +56,31 @@ int main() {
 			}
 			cout << "That is an invalid choice.\n";
 		}
+		
 
+		
 		if (cMenu == 'Y' || cMenu =='y') {
+			system("cls");
 			menu();
-			cout << "Please enter a number between 1 and 6\n";
-			while (!(cin >> ch) || ch > 7 || ch < 1) {
+			
+			while (!(cin >> ch) || ch > 6 || ch < 1) {
 				cinClear();
+				cout << "Please enter a number between 1 and 6\n";
 			}
 			doShape(ch);
 				
-		}
+		}	
 		else {
+			system("cls");
 			cout << "Please enter a number between 1 and 6\n";
-			while (!(cin >> ch) || ch > 7 || ch < 1) {
+			
+			while (!(cin >> ch) || ch > 6 || ch < 1) {				
 				cinClear();
 			}
 			doShape(ch);
 				
 		}
+		
 		cout << "\nDo you want to try another?\n\nPress 'Y'\n";
 		cin >> cAnotherOne;
 	}
@@ -82,19 +88,19 @@ int main() {
 		cout << "Thank you for using Shapes\n\n";
 
 	
-	system("Pause");
+	//system("Pause");
 	return 0;	
 }
 
-int doShape(int &ch) {
-	//Shape *shape;
+int doShape(int ch) {
+
 	Rectangle rectangle;
 	Circle circle;
 	Sector sector;
 	Triangle triangle;
 	Trapezoid trapazoid;
 	Square square;
-	double radius, angle;
+	double radius, angle, input;
 	int length, width, base, height, ShapeSide1, ShapeSide2, ShapeSide3;
 
 	
@@ -105,11 +111,12 @@ int doShape(int &ch) {
 	case 1: // Rectangle
 
 		cout << "Enter the width for the Rectangle.\n";
-		cin.clear();
-		cin >> width;
-		cout << " Enter the length for the Rectangle.\n";
-		cinClear();
-		cin >> length;
+		cin >> input;
+		width =checkIfPositive(input);
+
+		cout << "Enter the length for the Rectangle.\n";
+		cin >> input;
+		length = checkIfPositive(input);
 
 		cout << "The area of the Rectangle is " << rectangle.GetAreaRect(width, length) << endl;
 		cout << "The perimeter of the Rectangle is " << rectangle.GetPerimeter(width, length) << endl;
@@ -119,8 +126,8 @@ int doShape(int &ch) {
 	case 2: // circle
 
 		cout << "Enter the radius for the circle.\n";
-		cinClear();
-		cin >> radius;
+		cin >> input;
+		radius = checkIfPositive(input);
 
 		cout << "The perimeter of the circle " << circle.GetPerimeter(radius) << endl;
 		cout << "The area of the circle is " << circle.GetArea(radius) << endl;
@@ -130,11 +137,12 @@ int doShape(int &ch) {
 	case 3:// sector
 
 		cout << "Enter the radius for the sector.\n";
-		cinClear();
-		cin >> radius;
+		cin >> input;
+		radius = checkIfPositive(input);
+
 		cout << "Enter the angle of the sector.\n";
-		cinClear();
-		cin >> angle;
+		cin >> input;
+		angle = checkIfPositive(input);
 
 		cout << "\nThe area of the sector is " << sector.GetArea(radius) << endl;
 		cout << "The perimeter of the sector is " << sector.GetPerimeter(radius, angle) << endl;
@@ -142,17 +150,20 @@ int doShape(int &ch) {
 
 	case 4:// triangle
 		cout << "Enter the base for the triangle.\n";
-		cinClear();
-		cin >> base;
+		cin >> input;
+		base = checkIfPositive(input);
+
 		cout << "Enter the height for the triangle.\n";
-		cinClear();
-		cin >> height;
+		cin >> input;
+		height = checkIfPositive(input);
+
+		cout << "Enter side 1\n";
+		cin >> input;
+		ShapeSide1 = checkIfPositive(input);
+
 		cout << "Enter side 2\n";
-		cinClear();
-		cin >> ShapeSide1;
-		cout << "Enter side 3\n";
-		cinClear();
-		cin >> ShapeSide2;
+		cin >> input;
+		ShapeSide2= checkIfPositive(input);
 
 		cout << "The area of the triangle is " << triangle.GetArea(base, height) << endl;
 		cout << "The perimeter of the triangle is " << triangle.GetPerimeter(ShapeSide1, ShapeSide2, base) << endl;
@@ -160,28 +171,33 @@ int doShape(int &ch) {
 
 	case 5: // Trapezoid
 		cout << "Enter the base for the trapazoid\n";
-		cinClear();
-		cin >> base;
+		cin >> input;
+		base= checkIfPositive(input);
+
 		cout << "Enter the first side for the trapazoid\n";
-		cinClear();
-		cin >> ShapeSide1;
+		cin >> input;
+		ShapeSide1= checkIfPositive(input);
+
 		cout << "Enter the height for the trapazoid\n";
-		cinClear();
-		cin >> height;
+		cin >> input;
+		height= checkIfPositive(input);
+
 		cout << "Enter the second side for the trapazoid\n";
-		cinClear();
-		cin >> ShapeSide2;
+		cin >> input;
+		ShapeSide2= checkIfPositive(input);
+
 		cout << "Enter the third side for the trapazoid\n";
-		cinClear();
-		cin >> ShapeSide3;
+		cin >> input;
+		ShapeSide3= checkIfPositive(input);
 
 		cout << "The area of the trapazoid is " << trapazoid.getArea(base, ShapeSide1, height) << endl;
 		cout << "The perimeter of the trapazoid is " << trapazoid.getPerimeter(base, ShapeSide1, ShapeSide2, ShapeSide3) << endl;
 		break;
 	case 6:
 		cout << "Enter the length of the square\n";
-		cinClear();
-		cin >> length;
+		cin >> input;
+		length= checkIfPositive(input);
+	
 		cout << "The area of the square is " << square.GetArea(length) << endl;
 		cout << "The perimeter of the square is " << square.GetPerimeter(length) << endl;
 
@@ -190,21 +206,44 @@ int doShape(int &ch) {
 		//	default:
 			//	cout << "This is the end";
 	}
+	
 	return 0;
 }
 
 	void menu() {
 		
-	   cout << setw(25) << "\nPress '1' for Rectangle"
+	   cout << setw(23) << "Press '1' for Rectangle"
 			<< setw(25) << "Press '2' for Circle"
-			<< setw(28) << "Press '3' for sector"
-			<< setw(20) << "\nPress '4' for triangle"
+			<< setw(28) << "Press '3' for Sector"
+			<< setw(20) << "\nPress '4' for Triangle"
 			<< setw(29) << "Press '5' for Trapazoid"
-			<< setw(27) << "Press '6' for square\n\n";
+			<< setw(27) << "Press '6' for Square\n\n";
 	}
 
 	void cinClear() {
 		cin.clear();
 		cin.ignore(INT_MAX, '\n');
 	}
-	
+
+
+	double checkIfPositive(double input)
+	{
+		
+		while (input <= 0) {
+				if (input == 0) {
+					cout << "Number can't be 0\n";
+					while (input == 0) {
+						cin >> input;
+					}
+				}
+				else {
+					cout << "Enter a Positive number\n";
+					while (input < 0) {
+						cin >> input;
+					}
+				}
+		}
+		return input;
+	}
+
+
